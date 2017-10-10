4a5970ead0068cc257e17dc85130359b37080cda
lynxfikri <31949560+lynxfikri@users.noreply.github.com>
2017-10-10 11:43:36 +0800

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>LOGIN PAGE - Version1</title>
+	<title>LOGIN PAGE - V2</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
@@ -17,7 +17,7 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "LOGIN".($_ENV["NAME"]?$_ENV["NAME"]:"PAGE - VERSION 1")."!"; ?></h1>
+	<h1><?php echo "LOGIN".($_ENV["NAME"]?$_ENV["NAME"]:"PAGE - VERSION 2")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
